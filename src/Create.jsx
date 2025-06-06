264f6092dacc2e3cde8f2f29dc80606313ed5e03
Mc Roniel <ronieldegozo@gmail.com>
2025-06-06 18:33:38 +0800

    Temporary Change modal for create user

--- src/Create.jsx
+++ src/Create.jsx
@@ -22,7 +22,7 @@ function Create() {
     monthlySalary: values.monthlySalary.toString() // Send as string for backend BigDecimal
   };
 
-    axios.post('https://wallet-friendly.fly.dev/rest/v1/savings', payload)
+  axios.post('https://wallet-friendly.fly.dev/rest/v1/savings', payload)
       .then(response => {
         console.log("User created successfully:", response.data);
         navigate('/'); // Redirect to home page after successful creation
@@ -32,6 +32,8 @@ function Create() {
         console.error("Error creating user:", error);
       });
   }
+
+
   
   return (
     <div className='d-flex flex-column align-items-center justify-content-center vh-100'>
@@ -42,31 +44,31 @@ function Create() {
                 <label htmlFor="FirstName">FirstName: </label>
                 <input type="text" name="First Name" className='form-control' placeholder='Enter First Name' onChange={
                   (e) => setValues({ ...values, firstName: e.target.value })
-                }/>
+                } required/>
               </div>
               <div className="mb-2">
                 <label htmlFor="MiddleName">MiddleName: </label>
                 <input type="text" name="Middle Name" className='form-control' placeholder='Enter Middle Name' onChange={
                   (e) => setValues({ ...values, middleName: e.target.value })
-                }/>
+                } required/>
               </div>
               <div className="mb-2">
                 <label htmlFor="LastName">LastName: </label>
                 <input type="text" name="Last Name" className='form-control' placeholder='Enter Last Name' onChange={
                   (e) => setValues({ ...values, lastName: e.target.value })
-                }/>
+                } required/>
               </div>
               <div className="mb-2">
                 <label htmlFor="Occupation">Occupation: </label>
                 <input type="text" name="Occupation" className='form-control' placeholder='Enter Occupation' onChange={
                   (e) => setValues({ ...values, occupation: e.target.value })
-                }/>
+                } required/>
               </div>
               <div className="mb-2">
                 <label htmlFor="MonthlySalary">Monthly Salary: </label>
                 <input type="number" name="Monthly Salary" className='form-control' placeholder='Enter Monthly Salary' onChange={
                   (e) => setValues({ ...values, monthlySalary: e.target.value })
-                }/>
+                }required />
               </div>
               <button className='btn btn-success w-100 mt-3 mb-2' type="submit">
                 Create User
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 function Home() {
     const [data, setData] = useState([]);
@@ -38,11 +40,98 @@ function Home() {
             });
     }
 
+      const [show, setShow] = React.useState(false);
+    
+      const handleClose = () => setShow(false);
+      const handleShow = () => setShow(true);
+
+  const [values, setValues] = React.useState({
+    firstName: '',
+    middleName: '',
+    lastName: '',
+    occupation: '',
+    monthlySalary: ''
+  });
+
+
+  const handleSumit = (event) => {
+    event.preventDefault();
+    console.log("Form submitted with values:", values);
+
+  const payload = {
+    ...values,
+    monthlySalary: values.monthlySalary.toString() // Send as string for backend BigDecimal
+  };
+
+  axios.post('https://wallet-friendly.fly.dev/rest/v1/savings', payload)
+      .then(response => {
+        console.log("User created successfully:", response.data);
+        navigate('/'); // Redirect to home page after successful creation
+        alert("User created successfully!");
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error("Error creating user:", error);
+      });
+  }
+
+
+
     return (
     <div className='d-flex flex-column align-items-center justify-content-center vh-100'>
         <h1 className='text-center'>Savings</h1>
         <div className='d-flex justify-content-end w-100 shadow p-4'>
-           <Link to="/create" className="btn btn-success">Add User</Link>
+        <Button variant="primary" onClick={handleShow}>
+           Create User
+        </Button>
+        
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create New User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form  id="createUserForm"  onSubmit={handleSumit}>
+              <div className="mb-2">
+                <label htmlFor="FirstName">FirstName: </label>
+                <input type="text" name="First Name" className='form-control' placeholder='Enter First Name' onChange={
+                  (e) => setValues({ ...values, firstName: e.target.value })
+                } required/>
+              </div>
+              <div className="mb-2">
+                <label htmlFor="MiddleName">MiddleName: </label>
+                <input type="text" name="Middle Name" className='form-control' placeholder='Enter Middle Name' onChange={
+                  (e) => setValues({ ...values, middleName: e.target.value })
+                } required/>
+              </div>
+              <div className="mb-2">
+                <label htmlFor="LastName">LastName: </label>
+                <input type="text" name="Last Name" className='form-control' placeholder='Enter Last Name' onChange={
+                  (e) => setValues({ ...values, lastName: e.target.value })
+                } required/>
+              </div>
+              <div className="mb-2">
+                <label htmlFor="Occupation">Occupation: </label>
+                <input type="text" name="Occupation" className='form-control' placeholder='Enter Occupation' onChange={
+                  (e) => setValues({ ...values, occupation: e.target.value })
+                } required/>
+              </div>
+              <div className="mb-2">
+                <label htmlFor="MonthlySalary">Monthly Salary: </label>
+                <input type="number" name="Monthly Salary" className='form-control' placeholder='Enter Monthly Salary' onChange={
+                  (e) => setValues({ ...values, monthlySalary: e.target.value })
+                }required />
+              </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+            <Button variant="primary"  type="submit" form="createUserForm">
+            Save Changes
+            </Button>
+        </Modal.Footer>
+      </Modal>
         </div>
         <table className='table tavle-striped table-bordered'>
             <thead>
